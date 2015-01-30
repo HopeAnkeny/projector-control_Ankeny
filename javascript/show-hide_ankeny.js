2ff9b6aca4cff7273b2f8544e5847d74c4a2c8e4
Jared Brees <j8t8b@yahoo.com>
2015-01-30 10:45:58 -0600

    updated show/hide JS

--- javascript/show-hide_ankeny.js
+++ javascript/show-hide_ankeny.js
@@ -17,23 +17,33 @@ _ls = lens shift
 */
 
 
-function showBridge(id) {
-	hideStuff('mainTop');
-	hideBridge(id);
+// initialize vars so we don't
+// have to use quotes:
+
+var mainTop = 'mainTop';
+
+// Worship Center:
+var wcMainScreen = 'wcMainScreen';
+var wcFoldbackScreen = 'wcFoldbackScreen';
+var wcMainScreen_tc = 'wcMainScreen_tc';
+var wcMainScreen_mc = 'wcMainScreen_mc';
+var wcMainScreen_fz = 'wcMainScreen_fz';
+var wcMainScreen_ls = 'wcMainScreen_ls';
+var wcFoldbackScreen_tc = 'wcFoldbackScreen_tc';
+var wcFoldbackScreen_mc = 'wcFoldbackScreen_mc';
+var wcFoldbackScreen_fz = 'wcFoldbackScreen_fz';
+var wcFoldbackScreen_ks = 'wcFoldbackScreen_ks';
+var wcFoldbackScreen_ls = 'wcFoldbackScreen_ls';
+
+
+
+
+function showWC(id) {
+	hideStuff(mainTop);
+	hideWC(id);
 	document.getElementById(id).style.display = 'block';
 }
 
-function showChapel(id) {
-	hideStuff('mainTop');
-	hideChapel(id);
-	document.getElementById(id).style.display = 'block';
-}
-
-function showGym(id) {
-	hideStuff('mainTop');
-	hideGym(id);
-	document.getElementById(id).style.display = 'block';
-}
 
 
 /*
@@ -43,66 +53,20 @@ function showGym(id) {
 
 
 
-// start of "bridge"
-function hideBridge(id) {
-	hideStuff('bridgeMainLeft');
-	hideStuff('bridgeMainRight');
-	hideStuff('bridgeMainCenter');
-	hideStuff('bridgeFoldbackCenter');
-	hideStuff('bridgeMainLeft_tc');
-	hideStuff('bridgeMainLeft_mc');
-	hideStuff('bridgeMainLeft_fz');
-	hideStuff('bridgeMainLeft_ks');
-	hideStuff('bridgeMainLeft_ls');
-	hideStuff('bridgeMainRight_tc');
-	hideStuff('bridgeMainRight_mc');
-	hideStuff('bridgeMainRight_fz');
-	hideStuff('bridgeMainRight_ks');
-	hideStuff('bridgeMainRight_ls');
-	hideStuff('bridgeMainCenter_tc');
-	hideStuff('bridgeMainCenter_mc');
-	hideStuff('bridgeMainCenter_fz');
-	hideStuff('bridgeMainCenter_ks');
-	hideStuff('bridgeMainCenter_ls');
-	hideStuff('bridgeFoldbackCenter_tc');
-	hideStuff('bridgeFoldbackCenter_mc');
-	hideStuff('bridgeFoldbackCenter_fz');
-	hideStuff('bridgeFoldbackCenter_ks');
-	hideStuff('bridgeFoldbackCenter_ls');
+// start of "WC"
+function hideWC(id) {
+	hideStuff(wcMainScreen);
+	hideStuff(wcFoldbackScreen);
+	hideStuff(wcMainScreen_tc);
+	hideStuff(wcMainScreen_mc);
+	hideStuff(wcMainScreen_fz);
+	hideStuff(wcMainScreen_ls);
+	hideStuff(wcFoldbackScreen_tc);
+	hideStuff(wcFoldbackScreen_mc);
+	hideStuff(wcFoldbackScreen_fz);
+	hideStuff(wcFoldbackScreen_ks);
+	hideStuff(wcFoldbackScreen_ls);
 }
-// end of "bridge"
-
-// start of "chapel"
-function hideChapel(id) {
-	hideStuff('chapelMainCenter');
-	hideStuff('chapelMainSide');
-	hideStuff('chapelFoldbackSide');
-	hideStuff('chapelMainCenter_tc');
-	hideStuff('chapelMainCenter_mc');
-	hideStuff('chapelMainCenter_fz');
-	hideStuff('chapelMainCenter_ks');
-	hideStuff('chapelMainCenter_ls');
-	hideStuff('chapelMainSide_tc');
-	hideStuff('chapelMainSide_mc');
-	hideStuff('chapelMainSide_fz');
-	hideStuff('chapelMainSide_ls');
-	hideStuff('chapelFoldbackSide_tc');
-	hideStuff('chapelFoldbackSide_mc');
-	hideStuff('chapelFoldbackSide_fz');
-	hideStuff('chapelFoldbackSide_ks');
-	hideStuff('chapelFoldbackSide_ls');
-}
-// end of "chapel"
-
-// start of "gym"
-function hideGym(id) {
-	hideStuff('gymMainSide');
-	hideStuff('gymMainSide_tc');
-	hideStuff('gymMainSide_mc');
-	hideStuff('gymMainSide_fz');
-	hideStuff('gymMainSide_ks');
-	hideStuff('gymMainSide_ls');
-}
-// end of "gym"
+// end of "WC"
 
 // done - at least for now...;
